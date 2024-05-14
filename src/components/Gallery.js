5c2dca24ad99f3c8d0eafea93fe264c485da6866
dev-maaz <dev.maaztariq@gmail.com>
2024-05-14 11:08:04 +0500

    k "fixed routing errors"

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,25 +1,102 @@
 
 import "./Gallery.scss";
+import { Link } from "react-router-dom";
 
 function Gallery(){
 
+    const imageArray = 
+[
+  {
+    id: 1,
+    title: 'Clouds',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/simple-clouds.jpg",
+  },
+  {
+    id: 2,
+    title: 'Fire',
+    description: "Colorful fire flickered and danced, casting vibrant hues across the night sky like a painter's brush on a canvas. Each flame held a story, painting the air with an array of reds, oranges, blues, and greens, mesmerizing onlookers with its kaleidoscope of brilliance.",
+    imageUrl: "/simple-fire.jpg",
+  },
+  {
+    id: 3,
+    title: 'Calm Water',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/simple-waves.jpg",
+  },
+  {
+    id: 4,
+    title: 'Tranquility',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/still-water.jpg",
+  },
+  {
+    id: 5,
+    title: 'Mount Fuji',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/mountfuji-1.jpg",
+  },
+  {
+    id: 6,
+    title: 'Mountain Sunset',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/mountfuji-2.jpg",
+  },
+  {
+    id: 7,
+    title: 'Turbulent Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-1.jpg",
+  },
+  {
+    id: 8,
+    title: 'Great Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-2.jpg",
+  },
+  {
+    id: 9,
+    title: 'Thrashing Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-3.jpg",
+  },
+  {
+    id: 10,
+    title: 'Gigantic Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-4.jpg",
+  },
+  {
+    id: 11,
+    title: 'Perilous Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-5.jpg",
+  },
+  {
+    id: 12,
+    title: 'Beach',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/beach.jpg",
+  },
+  {
+    id: 13,
+    title: 'School of Fish',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/fish-school.jpg",
+  },
+  {
+    id: 14,
+    title: 'Temple',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/temple.jpg",
+  },
+];
     return <>
+
     <div className="grid grid-cols-5 p-8 gap-8 w-full">
-
-        <img src="simple1.jpg" className='picture-format w-full' alt="image1"/>
-        <img src="simple2.jpg" className='picture-format'alt="image2"/>
-        <img src="simple3.jpg" className='picture-format' alt="image3"/>
-        <img src="mountfuji2.jpg" className='picture-format' alt="image4"/>
-        <img src="simple5.jpg" className='picture-format' alt="image5"/>
-        <img src="simple 6.jpg" className='picture-format' alt="image6"/>
-        <img src="temple.jpg" className='picture-format' alt="image7"/>
-        <img src="wave1.jpg" className='picture-format' alt="image8"/>
-        <img src="wave 2.jpg" className='picture-format' alt="image9"/>
-        <img src="wave3.jpg" className='picture-format' alt="image10"/>
-        <img src="wave3.jpg" className='picture-format' alt="image11"/>
-        <img src="waveright.jpg" className='picture-format' alt="image12"/>
-
+        {imageArray.map((image) => <Link to={ `../image/${image.id}`}> <img src={image.imageUrl} className='picture-format w-full' alt={image.id} /> </Link>)}
     </div>
+
     </>
 }
 
--- src/components/ImagePage.js
+++ src/components/ImagePage.js
@@ -2,19 +2,92 @@
 import { useParams,  } from "react-router-dom";
 
 const imageArray = 
-[{
+[
+  {
     id: 1,
     title: 'Clouds',
     description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
-    imageUrl: "/simple 6.jpg",
+    imageUrl: "/simple-clouds.jpg",
   },
-{
-id: 2,
-title: 'Waves',
-description: "In rhythmic cadence, waves embrace the shoreline's edge, A symphony of motion, echoing the depths' pledge.",
-imageUrl: '/simple1.jpg',
-}];
-
+  {
+    id: 2,
+    title: 'Fire',
+    description: "Colorful fire flickered and danced, casting vibrant hues across the night sky like a painter's brush on a canvas. Each flame held a story, painting the air with an array of reds, oranges, blues, and greens, mesmerizing onlookers with its kaleidoscope of brilliance.",
+    imageUrl: "/simple-fire.jpg",
+  },
+  {
+    id: 3,
+    title: 'Calm Water',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/simple-waves.jpg",
+  },
+  {
+    id: 4,
+    title: 'Tranquility',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/still-water.jpg",
+  },
+  {
+    id: 5,
+    title: 'Mount Fuji',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/mountfuji-1.jpg",
+  },
+  {
+    id: 6,
+    title: 'Mountain Sunset',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/mountfuji-2.jpg",
+  },
+  {
+    id: 7,
+    title: 'Turbulent Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-1.jpg",
+  },
+  {
+    id: 8,
+    title: 'Great Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-2.jpg",
+  },
+  {
+    id: 9,
+    title: 'Thrashing Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-3.jpg",
+  },
+  {
+    id: 10,
+    title: 'Gigantic Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-4.jpg",
+  },
+  {
+    id: 11,
+    title: 'Perilous Waves',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/wave-5.jpg",
+  },
+  {
+    id: 12,
+    title: 'Beach',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/beach.jpg",
+  },
+  {
+    id: 13,
+    title: 'School of Fish',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/fish-school.jpg",
+  },
+  {
+    id: 14,
+    title: 'Temple',
+    description: 'In ephemeral grace, they veil the cerulean expanse,Whispering tales of ethereal realms to those below.',
+    imageUrl: "/temple.jpg",
+  },
+];
 
 const ImagePage = () => {
   const { imageId } = useParams();
